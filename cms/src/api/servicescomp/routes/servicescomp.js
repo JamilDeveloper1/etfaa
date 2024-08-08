ec416f44367b1127faa93187dfc99e18ac2fc06d
muradovjamil94@gmail.com <muradovjamil94@gmail.com>
2024-08-08 17:27:35 +0400

    deployed

---
+++ cms/src/api/servicescomp/routes/servicescomp.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * servicescomp router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::servicescomp.servicescomp');
---
+++ cms/src/api/servicescomp/services/servicescomp.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * servicescomp service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::servicescomp.servicescomp');
